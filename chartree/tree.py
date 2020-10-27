51bac3f757baa0e0f47e27e422cf9bdb09294056
Ben Somerville <70228881+bsmrvl@users.noreply.github.com>
2020-10-27 18:05:22 -0500

    cleaner code

--- chartree/tree.py
+++ chartree/tree.py
@@ -1,6 +1,4 @@
-"""
-Grow a tree in an Ecosystem object.
-"""
+"""Grow a tree in an Ecosystem object."""
 
 import numpy as np
 from IPython.display import clear_output
@@ -9,26 +7,35 @@ from time import sleep
 
 def vec_from_angle(from_vec, angle):
     from_vec = np.copy(from_vec)
-    arc = np.arctan(from_vec[1]/from_vec[0])*180/pi
-    if from_vec[0]<0:
-        big = 180+arc+angle
-    elif from_vec[1]<0:
-        big = 360+arc+angle
+    angle = angle * pi / 180
+    arc = np.arctan(from_vec[1] / from_vec[0])
+    if from_vec[0] < 0:
+        new_ang = pi + arc + angle
+    elif from_vec[1] < 0:
+        new_ang = 2*pi + arc + angle
     else:
-        big = arc+angle
-    big = pi*big/180
-    return np.array([np.cos(big), np.sin(big)])
+        new_ang = arc + angle
+    return np.array([np.cos(new_ang), np.sin(new_ang)])
+
+def split(br, ang, width_prop, ang_prop):
+    s_center = br.center
+    s_width = (width_prop * br.width**2)**(1/2)
+    s_direc = vec_from_angle(br.direction, ang_prop*ang)
+
+    br.width = ((1-width_prop) * br.width**2)**(1/2)
+    br.direction = vec_from_angle(br.direction, -(1-ang_prop)*ang)
+
+    return Branch(center=s_center, width=s_width, direction=s_direc)
 
 class Branch:
     def __init__(self, center, width, direction):
         self.center = np.array(center)
         self.width = width
-        self.direction = np.array(direction)/np.linalg.norm(np.array(direction))
-        self.thresh = .5
+        self.direction = np.array(direction) / np.linalg.norm(np.array(direction))
+        self.sign_proba = .5
 
 class Ecosystem:
-    """
-    A must for most trees. Set up your Ecosystem and then use grow() and show() methods to create your tree.
+    """A must for most trees. Set up your Ecosystem and then use grow() and show() methods to create your tree.
     
     Parameters
     ------------
@@ -45,22 +52,22 @@ class Ecosystem:
     """
 
     def __init__(self, size=[50,40], material='$', background='`'):
-        self.w = int(size[0]*2)
+        self.w = int(size[0] * 2)
         self.h = int(size[1])
-        self.mat = material
-        self.bg = background
+        self.mat = str(material)
+        self.bg = str(background)
         self.plot = 'Nothing has grown!'
         
     def make_wood(self, coor):
         coor = np.copy(coor)
-        coor[0] = coor[0]*2
+        coor[0] = coor[0] * 2
         coor = np.round(coor).astype(int)
-        if(coor[1]>=0 and coor[1]<self.h and coor[0]>=0 and coor[0]<self.w):
+        if (coor[1] >= 0 and coor[1] < self.h and 
+            coor[0] >= 0 and coor[0] < self.w):
             self.plot[coor[1], coor[0]] = 1
 
-    def show(self, material='', background=''):
-        """
-        Shows the grown tree. Can be used to experiment with characters without changing the shape of the tree. 
+    def show(self, material=None, background=None):
+        """Shows the grown tree. Can be used to experiment with characters without changing the shape of the tree. 
         
         Parameters:
         -------------
@@ -74,8 +81,10 @@ class Ecosystem:
         Nothing. Simply prints tree.
         """
 
-        if material: self.mat = material
-        if background: self.bg = background
+        if material is not None: 
+            self.mat = str(material)
+        if background is not None: 
+            self.bg = str(background)
         if isinstance(self.plot, str):
             print(self.plot)
         else:
@@ -83,17 +92,16 @@ class Ecosystem:
             for row in self.plot:
                 line = ''
                 for let in row:
-                    if let==1:
+                    if let == 1:
                         line += self.mat
                     else:
                         line += self.bg
-                whole += (line+'\n')
+                whole += (line + '\n')
             clear_output(wait=True)
             print(whole)
             
     def grow(self, trunk=3, n_iter=40, density=9, ang_mean=35, ang_range=5, watch=True, speed=.04):
-        """
-        Grows a tree, starting at the bottom center of the grid.
+        """Grows a tree, starting at the bottom center of the grid.
         
         Parameters:
         -------------
@@ -118,42 +126,43 @@ class Ecosystem:
         """
 
         self.plot = np.full(shape=(self.h, self.w), fill_value=0)
-        branches = [Branch(center=[self.w/4, self.h], width=trunk, direction=[.001,-7])]
+
+        branches = [Branch(center=[self.w/4, self.h], 
+                           width=trunk, 
+                           direction=[.001,-7])]
         
         for i in range(n_iter):
             hard_length = len(branches)
             for br in branches:
                 if branches.index(br) == hard_length:
                     break
+
                 for r in np.arange(0, br.width/2, .1):
-                    ring = np.array([1.,1.])
+                    ring = np.array([1., 1.])
                     ring -= ring.dot(br.direction) * br.direction
                     ring /= np.linalg.norm(ring)
                     ring *= r
-
                     self.make_wood(br.center + ring)
                     self.make_wood(br.center - ring)
 
                 if i%density == 0:
-                    ang = np.random.randint(ang_mean-ang_range, ang_mean+ang_range)\
-                                            * (1 if np.random.random() > br.thresh else -1)
-                    if ang<0:
-                        br.thresh -= .5
+                    ang = np.random.randint(ang_mean-ang_range, ang_mean+ang_range) \
+                          * (1 if np.random.random() > br.sign_proba else -1)
+                    if ang < 0:
+                        br.sign_proba -= .5
                     else:
-                        br.thresh += .5
+                        br.sign_proba += .5
 
-                    if i>.5*n_iter and br.width > .4:
-                        branches.append(Branch(center=br.center,
-                                               width=((br.width**2)*.5)**(1/2),
-                                               direction=vec_from_angle(br.direction,.5*ang)))
-                        br.direction = vec_from_angle(br.direction, -.5*ang)
-                        br.width = ((br.width**2)*.5)**(1/2)
+                    if i > .5*n_iter and br.width > .4:
+                        branches.append(split(br=br, 
+                                              ang=ang, 
+                                              width_prop=.5, 
+                                              ang_prop=.5))
                     else:
-                        branches.append(Branch(center=br.center, 
-                                               width=((br.width**2)*.2)**(1/2),
-                                               direction=vec_from_angle(br.direction,ang)))
-                        br.direction = vec_from_angle(br.direction, -.15*ang)
-                        br.width = ((br.width**2)*.8)**(1/2)
+                        branches.append(split(br=br, 
+                                              ang=ang, 
+                                              width_prop=.2, 
+                                              ang_prop=.9))
                 br.center = br.center + br.direction
                 br.width *= .97
             if watch:
@@ -161,4 +170,5 @@ class Ecosystem:
                 self.show()
 
     def __repr__(self):
-        return "Ecosystem of size {}x{}, material='{}', background='{}'".format(int(self.w/2), self.h, self.mat, self.bg)
+        return "Ecosystem of size {}x{}, material='{}', background='{}'" \
+               .format(int(self.w/2), self.h, self.mat, self.bg)
